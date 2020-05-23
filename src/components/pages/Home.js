6026fa2d3439f7e17f29ca27a59e037f70708dd5
Jonathan Castro <jcastrogarcia@gmail.com>
2020-05-22 21:05:02 -0400

    First implementation Google Places API.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,64 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Results from "../results/Results";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
 
-const Home = () => (
-  <Fragment>
-    <h1 className="text-center">Home page.</h1>
-    <br />
-    <div className="container">
-      <Results />
-    </div>
-  </Fragment>
-);
+const Home = () => {
+  const [address, setAddress] = useState("");
+  const [coordinates, setCoordinates] = useState({ lat: null, lng: null });
+
+  const handleSelect = async (value) => {
+    const results = await geocodeByAddress(value);
+    const latLng = await getLatLng(results[0]);
+    setAddress(value);
+    setCoordinates(latLng);
+  };
+
+  return (
+    <Fragment>
+      <h1 className="text-center">Home page.</h1>
+      <br />
+      <PlacesAutocomplete
+        value={address}
+        onChange={setAddress}
+        onSelect={handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <p>Latitude: {coordinates.lat}</p>
+            <p>Longitude: {coordinates.lng}</p>
+
+            <input {...getInputProps({ placeholder: "Type location" })} />
+
+            <div>
+              {loading ? <div>Loading...</div> : null}
+
+              {suggestions.map((suggestion) => {
+                const style = {
+                  backgroundColor: suggestion.active ? "#41b6e6" : "#fff",
+                };
+
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, { style })}
+                    key={suggestion.id}
+                  >
+                    {suggestion.description}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+      <br />
+      <div className="container">
+        <Results />
+      </div>
+    </Fragment>
+  );
+};
 
 export default Home;
---
+++ src/components/results/PlacesAutocomplete.js
@@ -0,0 +1,68 @@
+import React from "react";
+import userPlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import useOnclickOutside from "react-cool-onclickoutside";
+
+const PlacesAutocomplete = () => {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = userPlacesAutocomplete({
+    requestOptions: {},
+    debounce: 300,
+  });
+  const registerRef = useOnclickOutside(() => {
+    clearSuggestions();
+  });
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSelect = ({ description }) => () => {
+    setValue(description, false);
+    clearSuggestions();
+
+    getGeocode({ address: description })
+      .then((results) => getLatLng(results[0]))
+      .then(({ lat, lng }) => {
+        console.log("Coordinates: ", { lat, lng });
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  };
+
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
+
+      return (
+        <li key={id} onClick={handleSelect(suggestion)}>
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      );
+    });
+
+  return (
+    <div ref={registerRef}>
+      <input
+        value={value}
+        onChange={handleInput}
+        disabled={!ready}
+        placeholder="Enter or select a location to get info on it!"
+      />
+      {status === "OK" && <ul>{renderSuggestions()}</ul>}
+    </div>
+  );
+};
+
+export default PlacesAutocomplete;
