76fc86c0e5b0a1ac8658d06188ac3791cc56317c
Jonathan Castro <jcastrogarcia@gmail.com>
2020-06-07 00:04:50 -0400

    Updated About page text.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const About = () => {
-  return <div>TODO</div>;
+  return (
+    <div>
+      <h1 className="text-center">GeoInfo.cc</h1>
+      <h4 className="text-center">Find information about a location through API calls and React.</h4>
+      <hr/>
+      <p className="text-center">
+        This project is a study in using React to perform API calls and delivering them to a light front-end.
+      </p>
+    </div>
+  );
 };
 
 export default About;
