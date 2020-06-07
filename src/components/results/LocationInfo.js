34b167fdcff03c7a981e31c602d285d6be88b38a
Jonathan Castro <jcastrogarcia@gmail.com>
2020-06-06 20:19:27 -0400

    Work on displaying objects.

--- src/components/results/LocationInfo.js
+++ src/components/results/LocationInfo.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useState } from "react";
 import Accordion from "react-collapsy";
 
 import ResultsContext from "../../context/results/resultsContext";
@@ -8,12 +8,49 @@ require("../../../node_modules/react-collapsy/lib/index.css");
 const LocationInfo = () => {
   const resultsContext = useContext(ResultsContext);
   const { locationInfo } = resultsContext;
+  const list = [];
+
+  const [count, setCount] = useState(0);
+
+  const locationAccordionList = (locationInfo, list) => {
+    locationInfo
+      .filter((location) => {
+        if (location.properties.name === "") {
+          return false;
+        }
+        return true;
+      })
+      .map((location) => {
+        return list.push(
+          <Accordion key={location.id} title={location.properties.name}>
+            <p>
+              <strong>Kind: </strong>
+              {location.properties.kinds}
+            </p>
+          </Accordion>
+        );
+      });
+    console.log(list);
+  };
+
+  const showLocs = (count) => {
+    // let a;
+    // for (let count; count < count + 10; setCount(count++)) {
+    //   a = list[count];
+    // }
+    // return a;
+    return list[count]
+  };
+
+  const showMore = (count) => {
+    showLocs(count);
+  };
 
   return (
     <Fragment>
       <div>
         <div>
-          {locationInfo
+          {/* {locationInfo
             .filter((location) => {
               if (location.properties.name === "") {
                 return false;
@@ -29,7 +66,10 @@ const LocationInfo = () => {
                   </p>
                 </Accordion>
               );
-            })}
+            })} */}
+          {locationAccordionList(locationInfo, list)}
+          {showLocs(count)}
+          <button onClick={showMore(count)}>Show More</button>
         </div>
       </div>
     </Fragment>
